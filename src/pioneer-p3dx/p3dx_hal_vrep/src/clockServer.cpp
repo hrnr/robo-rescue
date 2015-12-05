7a9c4c0b6bf8d7ba33dfd47b9286515208a97f1b
Jiri Horner <laeqten@gmail.com>
2015-12-05 23:54:43 +0100

    vrep clock server: everything was wrong
    Simulator state was handled incorrectly. Mask was used as signed integer, mask compared to data etc. All was doomed to fail.
    
    Added comments explainig the whole situation to prevent such embarrasing situations in the future.

--- src/pioneer-p3dx/p3dx_hal_vrep/src/clockServer.cpp
+++ src/pioneer-p3dx/p3dx_hal_vrep/src/clockServer.cpp
@@ -4,20 +4,31 @@
 
 #include <string>
 
-const int SIM_STARTED = 1;
+constexpr unsigned int SIM_STARTED_MASK = 1;
 ros::Publisher clock_publisher;
 ros::Subscriber vrep_clock_subscriber;
 
 float sim_time = -1;
 
-void updateClock(const vrep_common::VrepInfo::ConstPtr & new_time) {
-  if (SIM_STARTED == new_time->simulatorState.data) {
-    sim_time = new_time->simulationTime.data;
-    rosgraph_msgs::Clock c;
-    c.clock.sec = static_cast<uint32_t>(sim_time);
-    c.clock.nsec = static_cast<uint32_t>((sim_time - c.clock.sec) * 1000000000);
-    clock_publisher.publish(c);
+void updateClock(const vrep_common::VrepInfo::ConstPtr& new_time) {
+  /*
+    simulatorState.data is bitwise encoded state
+    bit0 set: simulation not stopped
+    bit1 set: simulation paused
+    bit2 set: real-time switch on
+    bit3-bit5: the edit mode type (0=no edit mode, 1=triangle, 2=vertex, 3=edge, 4=path, 5=UI)
+  */
+  if (!(new_time->simulatorState.data & SIM_STARTED_MASK)) {
+    return;
   }
+
+  sim_time = new_time->simulationTime.data;
+  rosgraph_msgs::Clock c;
+  /* convert float time to sec a nano sec */
+  c.clock.sec = static_cast<uint32_t>(sim_time);
+  c.clock.nsec = static_cast<uint32_t>((sim_time - c.clock.sec) * 1000000000);
+
+  clock_publisher.publish(c);
 }
 
 int main(int argc, char **argv) {
